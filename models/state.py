b289e9ec3be47200f26d7f4a2a48b87a40212d25
CyrusNchege <cyruschegecc@gmail.com>
2023-05-14 05:08:27 -0700

    Add more models

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2
+"""
+model for state
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+        Class State inherits from BaseModel
+    """
+    name = ""
