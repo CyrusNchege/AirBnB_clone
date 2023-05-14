1ea72aa857742973f830a28e679c561f89b79354
CyrusNchege <cyruschegecc@gmail.com>
2023-05-14 03:04:32 -0700

    Modes more changes

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+models to implement city
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    class City inherits from BaseModel
+    """
+    state_id = ""
+    name = ""
